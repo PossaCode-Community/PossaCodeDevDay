a4171761dc23cbdba5b95cc646ecc089b876e9f5
sancty <sanctifieryawmensah55@gmail.com>
2024-05-23 18:27:19 +0200

    [add]: focus sur card de l'accordion

--- src/components/accordionSection.tsx
+++ src/components/accordionSection.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { useState } from "react";
 
 // Define the types for event items and events
 interface Speaker {
@@ -41,8 +42,9 @@ const events: Event[] = [
           "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "DIGITAL CONFERENCE INTRODUCTION",
         content: {
-          title: "Ubi est nobilis cobaltum? Domesticus capios ducunt ad demissio. Torquiss favere, tanquam bassus glos. Vae, hippotoxota! Superbus, mirabilis poetas vix aperto de teres, germanus amicitia. Vae. Fortis nixs ducunt ad acipenser. A falsis, musa nobilis gabalium. Cum decor ire, omnes fraticinidaes",
-        
+          title:
+            "Ubi est nobilis cobaltum? Domesticus capios ducunt ad demissio. Torquiss favere, tanquam bassus glos. Vae, hippotoxota! Superbus, mirabilis poetas vix aperto de teres, germanus amicitia. Vae. Fortis nixs ducunt ad acipenser. A falsis, musa nobilis gabalium. Cum decor ire, omnes fraticinidaes",
+
           speakers: [
             {
               name: "Jane Smith",
@@ -81,7 +83,6 @@ const events: Event[] = [
                 "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
           ],
-
         },
       },
       {
@@ -89,8 +90,9 @@ const events: Event[] = [
           "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "DIGITAL CONFERENCE INTRODUCTION",
         content: {
-          title: "Ubi est nobilis cobaltum? Domesticus capios ducunt ad demissio. Torquiss favere, tanquam bassus glos. Vae, hippotoxota! Superbus, mirabilis poetas vix aperto de teres, germanus amicitia. Vae. Fortis nixs ducunt ad acipenser. A falsis, musa nobilis gabalium. Cum decor ire, omnes fraticinidaes",
-        
+          title:
+            "Ubi est nobilis cobaltum? Domesticus capios ducunt ad demissio. Torquiss favere, tanquam bassus glos. Vae, hippotoxota! Superbus, mirabilis poetas vix aperto de teres, germanus amicitia. Vae. Fortis nixs ducunt ad acipenser. A falsis, musa nobilis gabalium. Cum decor ire, omnes fraticinidaes",
+
           speakers: [
             {
               name: "Jane Smith",
@@ -126,10 +128,9 @@ const events: Event[] = [
             {
               name: "Fiona Harris",
               photo:
-              "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
           ],
-
         },
       },
       // Add more items as needed
@@ -141,11 +142,13 @@ const events: Event[] = [
     localisation: "Summer C",
     items: [
       {
-        imgSrc:"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        imgSrc:
+          "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "DIGITAL CONFERENCE INTRODUCTION",
         content: {
-          title: "Ubi est nobilis cobaltum? Domesticus capios ducunt ad demissio. Torquiss favere, tanquam bassus glos. Vae, hippotoxota! Superbus, mirabilis poetas vix aperto de teres, germanus amicitia. Vae. Fortis nixs ducunt ad acipenser. A falsis, musa nobilis gabalium. Cum decor ire, omnes fraticinidaes",
-        
+          title:
+            "Ubi est nobilis cobaltum? Domesticus capios ducunt ad demissio. Torquiss favere, tanquam bassus glos. Vae, hippotoxota! Superbus, mirabilis poetas vix aperto de teres, germanus amicitia. Vae. Fortis nixs ducunt ad acipenser. A falsis, musa nobilis gabalium. Cum decor ire, omnes fraticinidaes",
+
           speakers: [
             {
               name: "Jane Smith",
@@ -166,7 +169,7 @@ const events: Event[] = [
             {
               name: "Alice Johnson",
               photo:
-                "/public/profile.png",
+                "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
             {
               name: "Bob Brown",
@@ -175,8 +178,7 @@ const events: Event[] = [
             },
             {
               name: "Charlie Davis",
-              photo:
-                "/public/profile.png",
+              photo: "/public/profile.png",
             },
             {
               name: "Fiona Harris",
@@ -184,7 +186,6 @@ const events: Event[] = [
                 "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
           ],
-
         },
       },
       // Add more items as needed
@@ -193,11 +194,31 @@ const events: Event[] = [
   // Add more events as needed
 ];
 export function AccordionDemo() {
+  const [focusedItem, setFocusedItem] = useState<{
+    eventIndex: number;
+    itemIndex: number;
+  } | null>(null);
+
+  const handleClick =(eventIndex, itemIndex)=>{
+
+    {
+      if (
+        focusedItem &&
+        focusedItem.eventIndex === eventIndex &&
+        focusedItem.itemIndex === itemIndex
+      ) {
+        setFocusedItem(null); // Si déjà cliqué, annuler le focus
+      } else {
+        setFocusedItem({ eventIndex, itemIndex }); // Sinon, mettre à jour le focus
+      }
+    }
+  }
+
   return (
-    <div className="container  grid grid-cols-1 gap-10 lg:grid-cols-2">
+    <div className="container grid grid-cols-1 gap-10 lg:grid-cols-2">
       {/* // Automatisation de l'accordion */}
       {events.map((event, eventIndex) => (
-        <div className="w-full border-2 border-black " key={eventIndex}>
+        <div className="w-full" key={eventIndex}>
           <div className="">
             <h1 className="font-bold text-3xl py-8 text-center">
               {event.date}
@@ -206,18 +227,24 @@ export function AccordionDemo() {
           <Accordion type="single" collapsible className="">
             {event.items.map((item, itemIndex) => (
               <AccordionItem key={itemIndex} value={`item-${itemIndex}`}>
-                {/* // image principal */}
-                <AccordionTrigger>
-                  <div className="flex flex-col gap-12 items-center md:flex-row">
+                <AccordionTrigger className="">
+                  <div className="flex flex-col gap-12 items-center  md:flex-row ">
                     <img
                       src={item.imgSrc}
                       alt="event"
                       className="size-28 rounded-full border-spacing-4 object-cover items-start"
                     />
-                    <div className="flex flex-col items-start space-y-2 text-left">
-                      <p className="text-red-700 tracking-widest uppercase text-base font-serif font-semibold">{event.hour}</p>{" "}
+                    <div
+                      className={`flex flex-col items-start space-y-2 text-left  py-8 px-10  ${focusedItem && focusedItem.eventIndex === eventIndex && focusedItem.itemIndex === itemIndex ? "bg-slate-100" : ""}`}
+                      onClick={()=>handleClick(eventIndex, itemIndex)}
+                    >
+                      <p className="text-red-700 tracking-widest uppercase text-base font-serif font-semibold">
+                        {event.hour}
+                      </p>{" "}
                       {/* Utiliser event.hour pour automatiser */}
-                      <h1 className="text-xl font-bold font-serif">{item.title}</h1>
+                      <h1 className="text-xl font-bold font-serif">
+                        {item.title}
+                      </h1>
                       <p className="text-gray-500 text-base">
                         by
                         <span className=" text-darkBlue"> Jessica Brown</span> -
@@ -229,9 +256,11 @@ export function AccordionDemo() {
 
                 {/*  // contenu de de l'evenement  */}
                 <AccordionContent>
-                  <div className="flex  flex-col flex-wrap items-center space-y-4 gap-x-24 md:pl-52">
-                    <p className="text-gray-500">{item.content.title}</p>
-                    <div className="flex -space-x-4 pl-0 -ml-4">
+                  <div className="flex flex-col space-y-4 md:pl-40">
+                    <div>
+                      <p className="text-gray-500 mb-4">{item.content.title}</p>
+                    </div>
+                    <div className="flex flex-wrap  -space-x-4 ">
                       {item.content.speakers.map((speaker, speakerIndex) => (
                         <div
                           key={speakerIndex}
@@ -240,8 +269,8 @@ export function AccordionDemo() {
                           <img
                             src={speaker.photo}
                             alt={speaker.name}
-                            className="size-14 rounded-full object-cover border-2 border-white group-hover:scale-150 transition-transform duration-300"
-                          /> 
+                            className="w-14 h-14 rounded-full object-cover border-2 border-white group-hover:scale-150 transition-transform duration-300"
+                          />
                         </div>
                       ))}
                     </div>
